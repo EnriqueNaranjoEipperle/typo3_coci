e4d1d5a58b644d56cf14dc596b38a44e0cbdcb61
Enrique Naranajo Eipperle <naranjohaus@web.de>
2026-01-20 11:11:18 +0100

    Remove vendor directory from repository

--- vendor/lochmueller/sourceopt/Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Add static template configuration
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'sourceopt',
-    'Configuration/TypoScript',
-    'Source Optimization'
-);
